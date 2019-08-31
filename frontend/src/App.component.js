a0ffcdd9ca5f5e4b62bd802a451ddd01f104379a
Scott Wagner <scottywags16@gmail.com>
2019-08-31 10:32:15 -0400

    show 'In Stock' if there are at least 6 items in stock

--- frontend/src/App.component.js
+++ frontend/src/App.component.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./App.css";
 
+const Quantity = ({ quantity }) =>
+  quantity >= 6 ? (
+    <div className="quantity inStock">In Stock</div>
+  ) : (
+    <div className="quantity">{quantity}</div>
+  );
+
 const App = ({ data }) => (
   <div className="App">
     <div className="logo">Logo Placeholder</div>
@@ -9,7 +16,7 @@ const App = ({ data }) => (
         <div className="productCard" key={index}>
           <h3>{productName}</h3>
           <div>{location}</div>
-          <div className="quantity">{quantity}</div>
+          <Quantity quantity={quantity} />
         </div>
       ))}
     </div>
