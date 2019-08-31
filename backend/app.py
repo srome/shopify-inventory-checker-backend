ec6362b3edf4d0674b692618e2219be5a2bfc093
Scott Wagner <scottywags16@gmail.com>
2019-08-31 00:11:34 -0400

    add CORS handling for flask
    add Pipfile for pipenv

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from inventory import Handler
 from json import dumps as to_json
 
 application = Flask(__name__)
+CORS(application)
 
 @application.route("/")
 def index():
